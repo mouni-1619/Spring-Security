3d9216fd58ead00965c6ccb03f393f2eb72e3234
mouni-1619 <mkoppolu07@gmail.com>
2025-06-23 14:58:16 +0530

    Add same class in next branch also

---
+++ src/main/java/org/tp/SpringSecurity1Application/Controller/ConflectPractice.java
@@ -0,0 +1,7 @@
+package org.tp.SpringSecurity1Application.Controller;
+
+public class ConflectPractice {
+    public static void main(String[] args) {
+        System.out.println("i add one class in next branch");
+    }
+}
